14b12b814ce1fdbc3555f53fa15f1d5fa543a321
Nicolas Paul <n@nc0.fr>
2024-03-04 11:38:44 +0100

    Configure ESLint
    ESLint is a popular linting framework and static analyzer in the
    JavaScript Ecosystem.  Our configuration follows the Google JavaScript
    Style Guide, as seen in issues.
    
    Closes: https://github.com/nc0fr/life2/issues/4
    
    Signed-off-by: Nicolas Paul <n@nc0.fr>

---
+++ .eslintrc.cjs
@@ -0,0 +1,62 @@
+// Copyright 2024 The Life2 Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview ESLint config following the Google style guide.
+ */
+
+/* eslint-env node */
+
+// Named constants for the numbers ESLint uses to indicate lint severity.
+const OFF = 0;
+const WARNING = 1;
+const ERROR = 2;
+
+// ESLint configuration object.  Options are described at
+// http://eslint.org/docs/user-guide/configuring.
+const ESLINT_CONFIG = {
+  extends : ['eslint-config-googlejs-es6'],
+  parserOptions : {
+    ecmaVersion : 6,
+    sourceType : 'module',
+  },
+  env : {
+    'node' : true,
+  },
+  globals : {},
+  plugins : [
+    'googlejs',
+  ],
+  // The list of rules and options are available at
+  // http://eslint.org/docs/rules/.
+  rules : {
+    // Disallow opt_ prefix and var_args as identifiers.
+    'googlejs/camelcase' : [
+      ERROR, {
+        allowVarArgs : false,
+        // TODO: disallow opt_ prefix once closure compiler stops warning about
+        // it.
+        allowOptPrefix : true,
+        allowLeadingUnderscore : true,
+        allowTrailingUnderscore : true,
+        checkObjectProperties : true,
+      }
+    ],
+  },
+  // ESLint supports adding shared settings into configuration file.  The
+  // settings object will be supplied to every rule that will be executed.
+  settings : {},
+};
+
+module.exports = ESLINT_CONFIG;
--- life2/simulator/src/game/world.js
+++ life2/simulator/src/game/world.js
@@ -21,15 +21,20 @@
  * @enum {string}
  */
 export const CELL_STATES = {
-    /** The cell is empty, possible because it never was filled or it died. */
-    EMPTY: ".",
-    /** The cell is occupied by a member of team A. */
-    TEAM_A: "a",
-    /** The cell is occupied by a member of team B. */
-    TEAM_B: "b",
-    /** The cell is a barrier, and it cannot be occupied by any entity,
-     * nor can it be modified by rules. */
-    BARRIER: "#",
+  /**
+     The cell is empty, possible because it never was filled or it
+     died.
+   */
+  EMPTY: '.',
+  /** The cell is occupied by a member of team A. */
+  TEAM_A: 'a',
+  /** The cell is occupied by a member of team B. */
+  TEAM_B: 'b',
+  /**
+   * The cell is a barrier, and it cannot be occupied by any entity,
+   * nor can it be modified by rules.
+   */
+  BARRIER: '#',
 };
 
 /**
@@ -43,94 +48,93 @@ export const CELL_STATES = {
  * simulation takes place.
  */
 export class World {
-    /**
-     * Create a new world with the given width and height.
-     * @constructor
-     * @param {number} width - The width of the world.
-     * @param {number} height - The height of the world.
-     */
-    constructor(width, height) {
-        // TODO(nc0) Support different types of boards, not just 2D
-
-        /**
-         * The width of the world board.
-         * @type {number}
-         * @readonly
-         */
-        this.width = width;
-        /**
-         * The height of the world board.
-         * @type {number}
-         * @readonly
-         */
-        this.height = height;
-        /**
-         * The board of the world, where all the entities live.
-         * @type {Board}
-         * @private
-         */
-        this.board = this.newBoard(width, height);
-    }
+  /**
+   * Create a new world with the given width and height.
+   * @constructor
+   * @param {number} width - The width of the world.
+   * @param {number} height - The height of the world.
+   */
+  constructor(width, height) {
+    // TODO(nc0) Support different types of boards, not just 2D
 
     /**
-     * Create a new board with the given width and height, initializing all
-     * the cells to be empty.
-     * @param {number} width - The width of the board.
-     * @param {number} height - The height of the board.
-     * @returns {Board} The new board.
+     * The width of the world board.
+     * @type {number}
+     * @readonly
      */
-    newBoard(width, height) {
-        return new Array(width).fill(CELL_STATES.EMPTY.repeat(height));
-    }
-
+    this.width = width;
     /**
-     * Returns the cell at the given coordinates.
-     * @param {number} x - The x coordinate of the cell.
-     * @param {number} y - The y coordinate of the cell.
-     * @returns {CELL_STATES | null} The cell at the given coordinates,
-     * or null if the coordinates are out of bounds.
+     * The height of the world board.
+     * @type {number}
+     * @readonly
      */
-    getCell(x, y) {
-        if (x < 0 || x > this.width ||
-            y < 0 || y > this.height) return null;
-
-        return this.board[x][y];
-    }
-
+    this.height = height;
     /**
-     * Returns the cells that are adjacent to the given coordinates.
-     * @param {number} x - The x coordinate of the cell.
-     * @param {number} y - The y coordinate of the cell.
-     * @returns {Array<(number, number)>} The cells that are adjacent to the given
-     * coordinates.
+     * The board of the world, where all the entities live.
+     * @type {Board}
+     * @private
      */
-    getNeighbors(x, y) {
-        // TODO(nc0): Avoid hardcoding the 8 neighbors, check the bounds, ...
-        return [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1),
-                (x - 1, y),   /* (x, y) */  (x + 1, y),
-                (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)]
-               .filter((x, y) => this.getCell(x, y) !== null);
+    this.board = this.newBoard(width, height);
+  }
+
+  /**
+   * Create a new board with the given width and height, initializing all
+   * the cells to be empty.
+   * @param {number} width - The width of the board.
+   * @param {number} height - The height of the board.
+   * @returns {Board} The new board.
+   */
+  newBoard(width, height) {
+    return new Array(width).fill(CELL_STATES.EMPTY.repeat(height));
+  }
+
+  /**
+   * Returns the cell at the given coordinates.
+   * @param {number} x - The x coordinate of the cell.
+   * @param {number} y - The y coordinate of the cell.
+   * @returns {CELL_STATES | null} The cell at the given coordinates,
+   * or null if the coordinates are out of bounds.
+   */
+  getCell(x, y) {
+    if (x < 0 || x > this.width || y < 0 || y > this.height) return null;
+
+    return this.board[x][y];
+  }
+
+  /**
+   * Returns the cells that are adjacent to the given coordinates.
+   * @param {number} x - The x coordinate of the cell.
+   * @param {number} y - The y coordinate of the cell.
+   * @returns {Array<(number, number)>} The cells that are adjacent to the given
+   * coordinates.
+   */
+  getNeighbors(x, y) {
+    // TODO(nc0): Avoid hardcoding the 8 neighbors, check the bounds, ...
+    return [
+      (x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x - 1, y),
+      /* (x, y) */ (x + 1, y), (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)
+    ].filter((x, y) => this.getCell(x, y) !== null);
+  }
+
+  /**
+   *
+   */
+  nextState() {
+    // TODO(nc0): Implement various rules for the next state
+    const newBoard = this.newBoard(this.width, this.height);
+
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.height; y++) {
+        const cell = this.getCell(x, y);
+        const neighbors = this.getNeighbors(x, y);
+        neighbors;
+
+        // Apply the rules to the cell and its neighbors
+        // and update the new board
+        newBoard[x][y] = cell;
+      }
     }
 
-    /**
-     *
-     */
-    nextState() {
-        // TODO(nc0): Implement various rules for the next state
-        const newBoard = this.newBoard(this.width, this.height);
-
-        for (let x = 0; x < this.width; x++) {
-            for (let y = 0; y < this.height; y++) {
-                const cell = this.getCell(x, y);
-                const neighbors = this.getNeighbors(x, y);
-                neighbors;
-
-                // Apply the rules to the cell and its neighbors
-                // and update the new board
-                newBoard[x][y] = cell;
-            }
-        }
-
-        this.board = newBoard;
-    }
+    this.board = newBoard;
+  }
 }
